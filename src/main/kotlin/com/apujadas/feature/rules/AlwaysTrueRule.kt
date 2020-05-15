cd55af730a659dca0ffaa72227444f2038aafce7
Andres Blas Pujadas <andres.pujadas@mercadolibre.com>
2020-05-15 20:12:39 -0300

    remove unnecessary annotation

--- src/main/kotlin/com/apujadas/feature/rules/AlwaysTrueRule.kt
+++ src/main/kotlin/com/apujadas/feature/rules/AlwaysTrueRule.kt
@@ -1,12 +1,9 @@
 package com.apujadas.feature.rules
 
 import com.apujadas.feature.FeatureContext
-import com.fasterxml.jackson.annotation.JsonTypeInfo
 
-@JsonTypeInfo(use= JsonTypeInfo.Id.NAME, include= JsonTypeInfo.As.PROPERTY, property="@type")
 class AlwaysTrueRule : Rule {
     override fun satisfies(context: FeatureContext): Boolean {
         return true
     }
-
 }
